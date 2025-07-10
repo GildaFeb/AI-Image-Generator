54491377b79a73cf8dd4d6f8e261a4ee12a35465
GildaFeb <76148559+GildaFeb@users.noreply.github.com>
2025-07-10 23:15:39 +0800

    Initial Commit

---
+++ app/(invisible)/item/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// This file is used to handle the invisible item page
+// It is a placeholder for future content or functionality
+// Folder structure: app/(invisible)/item/page.tsx
+// Purpose: To provide a basic structure for the invisible item page
+// Accessible via: /item
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
---
+++ app/product/product-details/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// basic routing in Next.js 13 with app directory
+// this file is used to handle dynamic routes
+// const ProductDetails = async({params}) => {
+//     const id = (await params).id;
+//   return (
+//     <div> ProductDetails {id}</div>
+//   )
+// }
+
+// export default ProductDetails
